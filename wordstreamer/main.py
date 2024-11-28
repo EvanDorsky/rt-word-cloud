c3bd993561250ec3367fe9aa5fc2e9b654b5d7fc
Evan Dorsky <evan.dorsky@icloud.com>
2024-11-27 21:37:48 -0500

    Stream clean words to client

--- wordstreamer/main.py
+++ wordstreamer/main.py
@@ -5,6 +5,7 @@
 import asyncio
 from websockets.asyncio.server import serve
 
+import re
 import io
 import time
 import sys
@@ -13,22 +14,46 @@ import threading
 
 filename = "test.log"
 
+def clean_token(token):
+  token_l = token.lower().strip()
+
+  # disqualify certain tokens
+  bad_chars = "[]"
+  if any(c in token_l for c in bad_chars):
+    return ''
+
+  return re.sub(r'[^a-zA-Z]', '', token_l)
+
+def process(s):
+  tokens = list(map(clean_token, s.split(' ')))
+
+  tokens_clean = [t for t in tokens if t]
+  return tokens_clean
+
 async def echo(websocket):
   with io.open(filename, "r", 1) as reader:
-    async for message in websocket:
+    laststuff = ""
+    # async for message in websocket:
+    while websocket:
       stuff = reader.read()
-      print(stuff)
-      await websocket.send(stuff)
+      newstuff = stuff[len(laststuff):]
+
+      words = process(newstuff)
+
+      for word in words:
+        await websocket.send(word)
+      # await websocket.send(newstuff)
+      time.sleep(1)
+
+      laststuff = stuff
 
 def stream():
   with io.open(filename, "wb") as writer:
     process = subprocess.Popen(["./stream"], cwd="../../whisper.cpp/", stdout=writer)
 
-
 async def main():
   async with serve(echo, "localhost", 8765) as server:
     await server.serve_forever()
-
 
 if __name__ == "__main__":
   streamer = threading.Thread(target=stream)
